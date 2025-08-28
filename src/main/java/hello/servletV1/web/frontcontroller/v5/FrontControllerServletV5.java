32e532a57f1e825d4115e9b7ecad02756347c788
jngbnss <jngbnmn@naver.com>
2025-08-28 14:43:43 +0900

    save

--- src/main/java/hello/servletV1/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/hello/servletV1/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -5,7 +5,11 @@ import hello.servletV1.web.frontcontroller.MyView;
 import hello.servletV1.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servletV1.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servletV1.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servletV1.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servletV1.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servletV1.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servletV1.web.frontcontroller.v5.adapter.ControllerV3handlerAdapter;
+import hello.servletV1.web.frontcontroller.v5.adapter.ControllerV4handlerAdapter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -40,10 +44,19 @@ public class FrontControllerServletV5 extends HttpServlet {
                 MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new
                 MemberListControllerV3());
+
+        //V4 추가
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new
+                MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new
+                MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new
+                MemberListControllerV4());
     }
 
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3handlerAdapter());
+        handlerAdapters.add(new ControllerV4handlerAdapter());
     }
      ///////////////////////////////////////////////////////////////////
 
